01c8f54e37cc9733dd95906dc71d8ec086e02ee4
Yuriy-vasylevsky <yuriy.vasylevsky@gmail.com>
2021-09-27 19:54:11 +0300

    Завершив всі дз

--- src/js/task1.js
+++ src/js/task1.js
@@ -1,11 +1,11 @@
-// const delay = ms => {
-//   // Твой код
-// };
+const delay = ms => {
+  return Promise.resolve(ms);
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
-console.log(1);
+delay(2000).then(logger);
+// console.log(delay(2000).then(logger));
+
+delay(1000).then(logger);
+delay(1500).then(logger);
